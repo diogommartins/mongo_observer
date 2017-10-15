dfe97822d37518341c1dbdbb00fdb203c517393a
diogommartins <magalhaesmartins@icloud.com>
2017-10-15 21:53:58 -0200

    Updating tests documentation

--- tests/integration/handlers/live_collection/test_max_operator.py
+++ tests/integration/handlers/live_collection/test_max_operator.py
@@ -6,13 +6,22 @@ from tests.integration.handlers.live_collection.base import \
 
 class MaxOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
                        asynctest.TestCase):
+    """
+    $max
+
+    The $max operator updates the value of the field to a specified value if
+    the specified value is greater than the current value of the field. The
+    $max operator can compare values of different types, using the BSON
+    comparison order.
+
+    Behavior
+
+    If the field does not exists, the $max operator sets the field to the
+    specified value.
+    """
     template_doc = {"highScore": 800, "lowScore": 200}
 
     async def test_it_updates_values_lower_than_max(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$max": {
                 "highScore": 950
@@ -27,10 +36,6 @@ class MaxOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
         )
 
     async def test_it_does_nothing_to_values_higher_than_max(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$max": {
                 "highScore": 700
@@ -45,10 +50,6 @@ class MaxOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
         )
 
     async def test_an_update_operation_with_max_lower_than_value_doesnt_result_into_a_oplog_entry(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$max": {
                 "highScore": 700
--- tests/integration/handlers/live_collection/test_min_operator.py
+++ tests/integration/handlers/live_collection/test_min_operator.py
@@ -6,13 +6,25 @@ from tests.integration.handlers.live_collection.base import \
 
 class MinOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
                        asynctest.TestCase):
+    """
+    $min
+
+    The $min updates the value of the field to a specified value if the
+    specified value is less than the current value of the field. The $min
+    operator can compare values of different types, using the BSON comparison
+    order.
+
+    Behavior
+
+    If the field does not exists, the $min operator sets the field to the
+    specified value.
+
+    For comparisons between values of different types, such as a number and
+    a null, $min uses the BSON comparison order.
+    """
     template_doc = {"highScore": 800, "lowScore": 200}
 
     async def test_it_updates_values_higher_than_min(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$min": {
                 "lowScore": 150
@@ -27,10 +39,6 @@ class MinOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
         )
 
     async def test_it_does_nothing_to_values_lower_than_min(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$min": {
                 "lowScore": 250
@@ -45,10 +53,6 @@ class MinOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
         )
 
     async def test_an_update_operation_with_min_higher_than_value_doesnt_result_into_a_oplog_entry(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection,
-                         {self.doc['_id']: self.doc})
-
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$min": {
                 "lowScore": 250
