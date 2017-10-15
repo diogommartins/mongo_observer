0f6140a4940c013fc35d5c8df00f6e2c03f95b12
diogommartins <magalhaesmartins@icloud.com>
2017-10-15 16:47:59 -0200

    Adding $mul operation tests

--- tests/integration/handlers/test_live_collection.py
+++ tests/integration/handlers/test_live_collection.py
@@ -236,3 +236,180 @@ class IncOperationTests(LiveCollectionFieldUpdateOperatorsTests,
                 }
             }
         )
+
+
+class MinOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
+                       asynctest.TestCase):
+    template_doc = {"highScore": 800, "lowScore": 200}
+
+    async def test_it_updates_values_higher_than_min(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$min": {
+                "lowScore": 150
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "highScore": 800, "lowScore": 150}
+        )
+
+    async def test_it_does_nothing_to_values_lower_than_min(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$min": {
+                "lowScore": 250
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "highScore": 800, "lowScore": 200}
+        )
+
+    async def test_an_update_operation_with_min_higher_than_value_doesnt_result_into_a_oplog_entry(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$min": {
+                "lowScore": 250
+            }
+        })
+
+        with asynctest.patch.object(self.observer.operation_handler,
+                                    'on_update') as on_update:
+            await self.observer.observe_changes()
+            on_update.assert_not_called()
+
+
+class MaxOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
+                       asynctest.TestCase):
+    template_doc = {"highScore": 800, "lowScore": 200}
+
+    async def test_it_updates_values_lower_than_max(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$max": {
+                "highScore": 950
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "highScore": 950, "lowScore": 200}
+        )
+
+    async def test_it_does_nothing_to_values_higher_than_max(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$max": {
+                "highScore": 700
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "highScore": 800, "lowScore": 200}
+        )
+
+    async def test_an_update_operation_with_max_lower_than_value_doesnt_result_into_a_oplog_entry(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$max": {
+                "highScore": 700
+            }
+        })
+
+        with asynctest.patch.object(self.observer.operation_handler,
+                                    'on_update') as on_update:
+            await self.observer.observe_changes()
+            on_update.assert_not_called()
+
+
+class MulOperatorTests(LiveCollectionFieldUpdateOperatorsTests,
+                       asynctest.TestCase):
+    """
+    $mul
+
+    Multiply the value of a field by a number. The field to update must contain
+    a numeric value.
+
+    Missing Field
+
+    If the field does not exist in a document, $mul creates the field and
+    sets the value to zero of the same numeric type as the multiplier.
+
+    Mixed Type
+
+    Multiplication with values of mixed numeric types (32-bit integer,
+    64-bit integer, float) may result in conversion of numeric type. For
+    multiplication with values of mixed numeric types, the following type
+    conversion rules apply:
+    """
+    template_doc = {"dog": "Xablau", "price": 10.99}
+
+    async def test_it_multiply_the_value_of_a_field(self):
+        await self.observer.observe_changes()
+        self.assertEqual(self.handler.collection,
+                         {self.doc['_id']: self.doc})
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$mul": {
+                "price": 1.25
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "dog": "Xablau", "price": 13.7375}
+        )
+
+    async def test_using_mul_to_non_existing_field_created_it_with_zero_value(self):
+        await self.collection.update_one({'_id': self.doc['_id']},
+                                         {"$unset": {"price": ""}})
+        await self.observer.observe_changes()
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "dog": "Xablau"}
+        )
+
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$mul": {
+                "price": 1.25
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']],
+            {"_id": self.doc['_id'], "dog": "Xablau", "price": 0}
+        )
