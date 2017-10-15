a8b6868598f359e5e2c65ac851bb64a8e4d5254e
diogommartins <magalhaesmartins@icloud.com>
2017-10-15 17:22:20 -0200

    Updating $set operator test cases

--- tests/integration/handlers/live_collection/test_set_operator.py
+++ tests/integration/handlers/live_collection/test_set_operator.py
@@ -6,10 +6,37 @@ from tests.integration.handlers.live_collection.base import \
 
 class SetOperationTests(LiveCollectionFieldUpdateOperatorsTests,
                         asynctest.TestCase):
-    async def test_it_reacts_to_set_update_operations(self):
-        await self.observer.observe_changes()
-        self.assertEqual(self.handler.collection, {self.doc['_id']: self.doc})
+    """
+    $set
 
+    The $set operator replaces the value of a field with the specified value.
+
+    Behavior
+
+    If the field does not exist, $set will add a new field with the specified
+    value, provided that the new field does not violate a type constraint. If
+    you specify a dotted path for a non-existent field, $set will create the
+    embedded documents as needed to fulfill the dotted path to the field.
+
+    If you specify multiple field-value pairs, $set will update or
+    create each field.
+    """
+
+    template_doc = {
+        "quantity": 500,
+        "dog": "Xablau",
+        "details": {
+            "model": "14Q3",
+            "make": "xyz"
+        },
+        "tags": ["coats", "outerwear", "clothing"],
+        "ratings": [
+            {"name": "foo", "rating": 0},
+            {"name": "eggs", "rating": 4}
+        ]
+    }
+
+    async def test_it_updates_current_fields_value(self):
         await self.collection.update_one({'_id': self.doc['_id']}, {
             "$set": {
                 "dog": "Xena"
@@ -19,3 +46,43 @@ class SetOperationTests(LiveCollectionFieldUpdateOperatorsTests,
         await self.observer.observe_changes()
         self.assertEqual(self.handler.collection[self.doc['_id']]['dog'],
                          "Xena")
+
+    async def test_it_creates_if_not_exists(self):
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$set": {
+                "details.dog": "Xena"
+            }
+        })
+
+        await self.observer.observe_changes()
+
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']]['details']['dog'],
+            "Xena"
+        )
+
+    async def test_it_updates_embeded_documents(self):
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$set": {
+                "details.model": "C3PO"
+            }
+        })
+
+        await self.observer.observe_changes()
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']]['details']['model'],
+            "C3PO"
+        )
+
+    async def test_it_set_elements_in_arrays(self):
+        await self.collection.update_one({'_id': self.doc['_id']}, {
+            "$set": {
+                "ratings.1.rating": 666
+            }
+        })
+
+        await self.observer.observe_changes()
+        self.assertEqual(
+            self.handler.collection[self.doc['_id']]['ratings'][1]['rating'],
+            666
+        )
